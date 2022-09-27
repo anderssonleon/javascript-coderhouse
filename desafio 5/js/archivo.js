5a968f490e704852ca3bafd6191e520724fffd41
anderssonleon <andresleon1412199@gmail.com>
2022-09-26 22:46:58 -0300

    desafio complementario

--- desafio 5/js/archivo.js
+++ desafio 5/js/archivo.js
@@ -1,30 +1,33 @@
-
-let productos =[]
-let carrito =[]
-const productTag = document.getElementById('article')
-const canasta = document.getElementById('container2')
-let comprasTotales = 0
-
-
 class Producto{
-    constructor(nombre,precio,id){
+    constructor(nombre,precio,id,cantidad){
         this.nombre = nombre
         this.precio = precio
         this.id =id
+        this.cantidad = cantidad
     }
 }
-const ahesivo = new Producto("Adhesivo para rostro",849,1)
-const paleta = new Producto("Paleta 18 sombras rubi rose",1490,2)
-const delineador = new Producto("Delineador de ojos de fibra",3196,3)
-const labialExfoliante = new Producto("Labial exfoliante",1598,4)
-const paletaIluminadora = new Producto("Paleta iluminadora",999,5)
+const ahesivo = new Producto("Adhesivo para rostro",849,1,1)
+const paleta = new Producto("Paleta 18 sombras rubi rose",1490,2,1)
+const delineador = new Producto("Delineador de ojos de fibra",3196,3,1)
+const labialExfoliante = new Producto("Labial exfoliante",1598,4,1)
+const paletaIluminadora = new Producto("Paleta iluminadora",999,5,1)
 const setAriel = new Producto("Set ariel ",2399,6)
-const mascaraDeColageno = new Producto("Mascara colageno ",987,7)
+const mascaraDeColageno = new Producto("Mascara colageno ",987,7,1)
 const mascaraDapop = new Producto("Mascara 2 en DAPOP ",1796,8)
-const comboPaletas = new Producto("Combo 3 paletas de sombras de ojos",2699,9)
-const mascaraDescongestiva = new Producto("Mascara descongestiva ",1200,10)
-const aguaMiselar = new Producto("Agua miselar ",900,11)
-const delineadorLiquido = new Producto("delineador liquido",550,12)
+const comboPaletas = new Producto("Combo 3 paletas de sombras de ojos",2699,9,1)
+const mascaraDescongestiva = new Producto("Mascara descongestiva ",1200,10,1)
+const aguaMiselar = new Producto("Agua miselar ",900,11,1)
+const delineadorLiquido = new Producto("delineador liquido",550,12,1)
+
+
+
+let productos =[]
+let carrito =[]
+let comprasTotales = 0
+const contenedorProductos = document.getElementById('carritoContenedor')
+const contenedorCarrito = document.getElementById('carrito-contenedor')
+
+
 
 productos.push(ahesivo);
 productos.push(paleta);
@@ -42,53 +45,46 @@ productos.push(delineadorLiquido);
 productos.forEach(producto=>{
     const contenedor = document.createElement('div')
     contenedor.className = `card col-lg-4 col-md-6 col-sm-6 mt-5`
+    contenedor.innerHTML = `
+    <div class="card-body">
+    <h5 class="card-title">${producto.nombre}
+    ${producto.precio}</h5>
+    <button id="${producto.id}" class="btn w-50  btn btn-outline-info">AGREGAR</button>
+    </div>
+    `  
+  
+   contenedorProductos.appendChild(contenedor)
 
-    const contenedorHijo = document.createElement('div')
-    contenedorHijo.className = `card-body`
-    contenedor.appendChild(contenedorHijo)
-
-    const titleName = document.createElement('h5')
-    titleName.className = `card-title`
-    titleName.innerText =`${producto.nombre} 
-    $${producto.precio}`
-    contenedor.appendChild(titleName)
-    
-    
-    const button =  document.createElement('button')
-    button.className = 'btn btn-primary w-50'
-    button.innerText = `AGREGAR`
-    contenedor.appendChild(button)
  
-    productTag.append(contenedor)
-    
-    
-    button.onclick = ()=>{
-      let selection =  producto;
-      carrito.push(selection)
-      console.log(carrito)
-    }
+   const button = document.getElementById(`${producto.id}`)
 
+
+   button.addEventListener('click',()=>{
+       agregarCarrito(producto.id)
+      })
+
+  
 })
 
-const button2 =  document.createElement('button')
-    button2.className = 'btn btn-warning'
-    button2.innerText = `TERMINAR COMPRA`
-    canasta.append(button2)
+/*OPERADORES AVANZADOS*/ 
+const agregarCarrito = (prodId) =>{
+    const item = productos.find((prod) => prod.id === prodId)
+    carrito.push(item)
+    actualizarCarrito()
+    console.log(carrito)
+}
 
-    button2.onclick = ()=>{
-        carrito.forEach((prod)=>{
-            comprasTotales = comprasTotales + prod.precio
-        })
-         alert(`compras totales: $${comprasTotales}`)
-      }
+const actualizarCarrito = ()=>{
+    contenedorCarrito.innerHTML =""
 
+    carrito.forEach((prod)=>{
+        const div = document.createElement('div')
+        div.className = ('productoEnCarrito')
+        div.innerHTML =`
+        <p>precio:$${prod.precio}</p><p>${prod.nombre}</p><p><span id="cantidad">${prod.cantidad}</span></p> <button class="boton-eliminar"><i class="fas fa-trash-alt"></i></button>
+        `
+        contenedorCarrito.appendChild(div)
+    })
 
-
-
-
-
-/*DESDE ACA CODIGO DEL CARRITO DE COMPRAS PARA AGREAGAR UN ELEMENTO USAR EL CONSTRUCTOR */
-
-
-
+}
 
