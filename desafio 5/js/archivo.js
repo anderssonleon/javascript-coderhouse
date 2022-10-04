c3b9d10a018438d3cfa67bfd4fdb0ce9aabef3be
anderssonleon <andresleon1412199@gmail.com>
2022-10-03 23:48:29 -0300

    refuerzo

--- desafio 5/js/archivo.js
+++ desafio 5/js/archivo.js
@@ -26,8 +26,9 @@ let carrito =[]
 let comprasTotales = 0
 const contenedorProductos = document.getElementById('carritoContenedor')
 const contenedorCarrito = document.getElementById('carrito-contenedor')
-
-
+const botonVaciar = document.getElementById('vaciar-carrito')
+const contadorCarrito = document.getElementById('contadorCarrito')
+const precioTotal = document.getElementById('precioTotal')
 
 productos.push(ahesivo);
 productos.push(paleta);
@@ -41,6 +42,20 @@ productos.push(comboPaletas);
 productos.push(mascaraDescongestiva);
 productos.push(aguaMiselar);
 productos.push(delineadorLiquido);
+
+
+document.addEventListener('DOMContentLoaded',()=>{
+    if(localStorage.getItem('carrito')){
+        carrito = JSON.parse(localStorage.getItem('carrito'))
+        actualizarCarrito()
+    }
+})
+
+
+botonVaciar.onclick = ()=>{
+    carrito.length = 0
+    actualizarCarrito()
+}
 
 productos.forEach(producto=>{
     const contenedor = document.createElement('div')
@@ -68,24 +83,49 @@ productos.forEach(producto=>{
 
 /*OPERADORES AVANZADOS*/ 
 const agregarCarrito = (prodId) =>{
+   const existe = carrito.some(prod => prod.id === prodId)
+
+  if (existe) {
+    const prod = carrito.map(prod =>{
+        if (prod.id === prodId) {
+            prod.cantidad++
+        }
+    })
+  }else{
     const item = productos.find((prod) => prod.id === prodId)
     carrito.push(item)
-    actualizarCarrito()
     console.log(carrito)
+
+}
+    
+    actualizarCarrito()
     swal("Producto agregado!", "producto en carrito!", "success");
 }
 
+
+const eliminarCarrito = (prodId)=>{
+    const item = carrito.find((prod)=> prod.id === prodId)
+    const indice = carrito.indexOf(item) 
+    carrito.splice(indice,1)
+    actualizarCarrito()
+}
+
+
 const actualizarCarrito = ()=>{
     contenedorCarrito.innerHTML =""
-
     carrito.forEach((prod)=>{
         const div = document.createElement('div')
         div.className = ('productoEnCarrito')
         div.innerHTML =`
-        <p>precio:$${prod.precio}</p><p>${prod.nombre}</p><p><span id="cantidad">${prod.cantidad}</span></p> <button class="boton-eliminar"><i class="fas fa-trash-alt"></i></button>
+        <p>precio:$${prod.precio}</p><p>${prod.nombre}</p><p><span id="cantidad">${prod.cantidad}</span></p> <button onclick="eliminarCarrito(${prod.id})" class="boton-eliminar"><i class="fas fa-trash-alt"></i></button>
         `
         contenedorCarrito.appendChild(div)
+
+        localStorage.setItem('carrito',JSON.stringify(carrito))
     })
 
+     contadorCarrito.innerText = carrito.length
+     precioTotal.innerText = carrito.reduce((acc , prod) => acc + prod.precio,0)
 }
+
 
--- desafio 5/js/formulario.js
+++ desafio 5/js/formulario.js
@@ -2,41 +2,108 @@ const divDatos = document.getElementById('divDataId');
 const inputNombre = document.getElementById('nombre');
 const inputUsuario = document.getElementById('usuario');
 const inputEmail = document.getElementById('email');
-const botonWelcome = document.getElementById('botton');
+const btn = document.getElementById('botton');
 const divSaludo =document.getElementById('welcome')
+const expresion = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 
-botonWelcome.onclick = () =>{
-     
-    const usuario={
-        usuario:inputUsuario.value,
-        nombre:inputNombre.value,
-        email:inputEmail.value
+
+
+eventos()
+
+
+
+function eventos() {
+    document.addEventListener('DOMContentLoaded',btnDisabled)
+
+
+    inputNombre.addEventListener('blur',formularioEnviar)
+    inputUsuario.addEventListener('blur',formularioEnviar)
+    inputEmail.addEventListener('blur',formularioEnviar)
+    divDatos.addEventListener('submit',enviarEmail)
+}
+function btnDisabled() {
+    btn.disabled = true;
+}
+
+function formularioEnviar(e) {
+   if (e.target.value.length > 0) {
+    const error = document.querySelector('p.error')
+    if (error) {
+        error.remove()
     }
-localStorage.setItem('usuarioStorage', JSON.stringify(usuario));
-divDatos.remove()
+    e.target.classList.add('valido')
+   }else{
+    e.target.classList.remove('valido')
+     e.target.classList.add('invalido')
+     mensajeError(`llenar todos los campos para continuar`)
+   }
 
-welcome(usuario)
+   if (e.target.type === 'email') {
+      if (expresion.test(e.target.value)){
+        const error = document.querySelector('p.error')
+          if (error) {
+            error.remove()
+         }
+        e.target.classList.add('valido')
+      }else{
+        e.target.classList.remove('valido')
+        e.target.classList.add('invalido')
+        mensajeError('email no es valido!')
+      }
+   }
+
+  if (expresion.test(inputEmail.value) !== '' && inputNombre.value !== '' && inputUsuario !== '') {
+    btn.disabled = false
+   
+    }
+
 }
 
 
+function mensajeError(mensaje) {
+    const errorMensaje = document.createElement('p')
+    errorMensaje.textContent = mensaje
+    errorMensaje.classList.add('mensajeInvalido','error')
+
+
+    const errores = document.querySelectorAll('.error')
+       
+        if (errores.length === 0) {
+            divDatos.appendChild(errorMensaje)
+        }
+}
+function enviarEmail(e) {
+    e.preventDefault()
+    const loader = document.getElementById('spinner')
+    loader.style.opacity = 1
+
+    setTimeout(()=>{
+        loader.style.opacity = 0
+        divDatos.remove()
+        const usuario={
+            usuario:inputUsuario.value,
+            nombre:inputNombre.value,
+            email:inputEmail.value
+        }
+        localStorage.setItem('usuarioStorage', JSON.stringify(usuario));
+    
+        welcome(usuario)
+    },2000
+    )
+}
 function welcome(user) {
-    const saludarTitle = document.createElement('h2')
+    const saludarTitle = document.createElement('div')
+    saludarTitle.innerHTML =`
+    <div class="card" style="width: 100%;">
+  <img src="./photos/usuario.png" class="card-img-top" alt="imgUsuario">
+  <div class="card-body">
+    <h5 class="card-title">BIENVENIDO!</h5>
+    <p class="card-text">Hola! ${user.usuario} eres parte de nuestra comunidad!</p>
+    <a href="carrito.html"><button class="btn w-50  btn btn-outline-info">bienvenido</button></a>
+       </div>
+     </div>
+    `
 
-
-    saludarTitle.innerText = (`Eres parte de nuestras comunidad! te damos la bienvenida ${user.usuario}, explora nuestra tienda dandole continuar al boton`)
     divSaludo.append(saludarTitle)
-   
-    const bottonSaludar = document.createElement('button')
-    bottonSaludar.classList.add("bottn__carrito")
-
-
-    const alinck = document.createElement('a')
-    alinck.setAttribute('href',"carrito.html")
-    alinck.textContent = ('continuar')
-    bottonSaludar.append(alinck)
-
-   
-
-    divSaludo.append(bottonSaludar)
     
 }
